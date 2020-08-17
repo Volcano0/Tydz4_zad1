c13ae2abbf66a6a39b69e2efb6c2610e177b5ea0
Gabriel Cwik <gabriel.cwik@cgi.com>
2020-08-17 15:49:53 +0200

    add project files

---
+++ Tydz4_zad1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tydz4_zad1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Podaj liczbę");
+                var number = int.Parse(Console.ReadLine());
+
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("Liczba parzysta");
+                }
+                else
+                {
+                    Console.WriteLine("Liczba nieparzysta");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Podano nie liczby całkowite");
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
